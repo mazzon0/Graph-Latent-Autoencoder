d5222e3867d705763244b449f442458ff6d50d34
mazzon0 <danielemazzon2004@gmail.com>
2026-05-02 16:57:46 +0200

    implemented cnn autoencoder

---
+++ models/cnn_autoencoder.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn as nn
+
+class CnnAutoencoder(nn.Module):
+    """
+    A Deep Convolutional Autoencoder with a fully-connected MLP bottleneck.
+    """
+    def __init__(self, image_shape: list, channels: list, mlp_sizes: list):
+        """
+        Args:
+            image_shape (list): [C, H, W] of the input image. 
+                Constraints: H and W must be powers of 2.
+            channels (list): List of filter counts for the CNN.
+                N channels create N-1 CNN layers (3x3 kernel, stride=2).
+                Example: [3, 16, 32, 64]. First element must match image_shape[0].
+            mlp_sizes (list): List of hidden units for the MLP bottleneck.
+                Example: [1024, 512, 256]. First element must match the 
+                flattened output of the final CNN encoder layer.
+        """
+        super().__init__()
+        assert(image_shape[0] == channels[0])   # The CNN input channels do not meet the image channels
+        assert((image_shape[1] & (image_shape[1] - 1)) == 0)    # Image height is not a power of 2
+        assert((image_shape[2] & (image_shape[2] - 1)) == 0)    # Image width is not a power of 2
+        self.image_shape = image_shape
+        assert(channels[-1] * self.image_shape[1] // 2**(len(channels)-1) * self.image_shape[2] // 2**(len(channels)-1) == mlp_sizes[0])    # The CNN output size does not meet the MLP input size
+
+        # CNN Encoder
+        self.cnn_encoder = nn.Sequential()
+        for i in range(len(channels) - 1):
+            self.cnn_encoder.add_module(f"enc_conv_{i}", nn.Conv2d(channels[i], channels[i+1], kernel_size=3, stride=2, padding=1))
+            if i == len(channels) - 1 and len(mlp_sizes) == 0:
+                self.cnn_encoder.add_module(f"enc_conv_bn_{i}", nn.BatchNorm2d(channels[i+1]))
+                self.cnn_encoder.add_module(f"enc_conv_act_{i}", nn.LeakyReLU())
+
+        # MLP Encoder
+        self.mlp_encoder = nn.Sequential(nn.Flatten())
+        for i in range(len(mlp_sizes) - 1):
+            self.mlp_encoder.add_module(f"enc_mlp_{i}", nn.Linear(in_features=mlp_sizes[i], out_features=mlp_sizes[i+1]))
+
+            if i != len(mlp_sizes)-2:
+                self.mlp_encoder.add_module(f"enc_mlp_act_{i}", nn.LeakyReLU())
+
+        # MLP Decoder
+        self.mlp_decoder = nn.Sequential()
+        for i in range(len(mlp_sizes)-1, 0, -1):
+            self.mlp_decoder.add_module(f"dec_mlp_{i}", nn.Linear(in_features=mlp_sizes[i], out_features=mlp_sizes[i-1]))
+            self.mlp_decoder.add_module(f"dec_mlp_act_{i}", nn.LeakyReLU())
+        self.mlp_decoder.add_module(nn.Unflatten(1,
+            [channels[-1], self.image_shape[1] // 2**(len(channels)-1), self.image_shape[2] // 2**(len(channels)-1)]))
+
+        # CNN Decoder
+        self.cnn_decoder = nn.Sequential()
+        for i in range(len(channels)-1, 0, -1):
+            self.cnn_decoder.add_module(f"dec_tconv_{i}",
+                nn.ConvTranspose2d(channels[i], channels[i-1], kernel_size=3, stride=2, padding=1, output_padding=1))
+            
+            if i != 1:  # every layer has a Leaky ReLU activation, but the last one sigmoid activation for normalized values (0, 1)
+                self.cnn_decoder.add_module(f"dec_tconv_bn_{i}", nn.BatchNorm2d(channels[i-1]))
+                self.cnn_decoder.add_module(f"dec_tconv_act_{i}", nn.LeakyReLU())
+            else:
+                self.cnn_decoder.add_module(f"dec_tconv_act_{i}", nn.Sigmoid())
+            
+    def forward(self, x: torch.Tensor):
+        """
+        Passes the input through the full autoencoder pipeline.
+        
+        Args:
+            x (torch.Tensor): Input image batch of shape (N, C, H, W).
+            
+        Returns:
+            torch.Tensor: Reconstructed image batch of shape (N, C, H, W).
+        """
+        assert(self.image_shape[0] == x.shape[1])
+        assert(self.image_shape[1] == x.shape[2])
+        assert(self.image_shape[2] == x.shape[3])
+
+        x = self.cnn_encoder(x)
+        x = self.mlp_encoder(x)
+        x = self.mlp_decoder(x)
+        x = self.cnn_decoder(x)
+        return x
