84ac1f68a3f4d10791a7cf9b7981b1b63d579f9a
mazzon0 <danielemazzon2004@gmail.com>
2026-05-09 11:55:37 +0200

    fix encoder activation + bce loss compatibility

--- models/cnn_autoencoder.py
+++ models/cnn_autoencoder.py
@@ -1,11 +1,12 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class CnnAutoencoder(nn.Module):
     """
     A Deep Convolutional Autoencoder with a fully-connected MLP bottleneck.
     """
-    def __init__(self, image_shape: list, channels: list, mlp_sizes: list):
+    def __init__(self, image_shape: list, channels: list, mlp_sizes: list, train_with_sigmoid: bool = True):
         """
         Args:
             image_shape (list): [C, H, W] of the input image. 
@@ -16,6 +17,10 @@ class CnnAutoencoder(nn.Module):
             mlp_sizes (list): List of hidden units for the MLP bottleneck.
                 Example: [1024, 512, 256]. First element must match the 
                 flattened output of the final CNN encoder layer.
+            train_with_sigmoid (bool): Validation always applies sigmoid at the end.
+                Training applies sigmoid depending on this value.
+                Example: nn.BCEWithLogits loss needs the logits during the training phase,
+                requiring this parameter set to False.
         """
         super().__init__()
         assert(image_shape[0] == channels[0])   # The CNN input channels do not meet the image channels
@@ -23,12 +28,13 @@ class CnnAutoencoder(nn.Module):
         assert((image_shape[2] & (image_shape[2] - 1)) == 0)    # Image width is not a power of 2
         self.image_shape = image_shape
         assert(channels[-1] * self.image_shape[1] // 2**(len(channels)-1) * self.image_shape[2] // 2**(len(channels)-1) == mlp_sizes[0])    # The CNN output size does not meet the MLP input size
+        self.train_with_sigmoid = train_with_sigmoid
 
         # CNN Encoder
         self.cnn_encoder = nn.Sequential()
         for i in range(len(channels) - 1):
             self.cnn_encoder.add_module(f"enc_conv_{i}", nn.Conv2d(channels[i], channels[i+1], kernel_size=3, stride=2, padding=1))
-            if i == len(channels) - 1 and len(mlp_sizes) == 0:
+            if i != len(channels) - 2 or len(mlp_sizes) != 0:
                 self.cnn_encoder.add_module(f"enc_conv_bn_{i}", nn.BatchNorm2d(channels[i+1]))
                 self.cnn_encoder.add_module(f"enc_conv_act_{i}", nn.LeakyReLU())
 
@@ -38,12 +44,14 @@ class CnnAutoencoder(nn.Module):
             self.mlp_encoder.add_module(f"enc_mlp_{i}", nn.Linear(in_features=mlp_sizes[i], out_features=mlp_sizes[i+1]))
 
             if i != len(mlp_sizes)-2:
+                self.mlp_encoder.add_module(f"enc_mlp_bn_{i}", nn.BatchNorm1d(mlp_sizes[i+1]))
                 self.mlp_encoder.add_module(f"enc_mlp_act_{i}", nn.LeakyReLU())
 
         # MLP Decoder
         self.mlp_decoder = nn.Sequential()
         for i in range(len(mlp_sizes)-1, 0, -1):
             self.mlp_decoder.add_module(f"dec_mlp_{i}", nn.Linear(in_features=mlp_sizes[i], out_features=mlp_sizes[i-1]))
+            self.mlp_decoder.add_module(f"dec_mlp_bn_{i}", nn.BatchNorm1d(mlp_sizes[i-1]))
             self.mlp_decoder.add_module(f"dec_mlp_act_{i}", nn.LeakyReLU())
         self.mlp_decoder.add_module('unflatten',
             nn.Unflatten(1, [channels[-1], self.image_shape[1] // 2**(len(channels)-1), self.image_shape[2] // 2**(len(channels)-1)]))
@@ -57,8 +65,6 @@ class CnnAutoencoder(nn.Module):
             if i != 1:  # every layer has a Leaky ReLU activation, but the last one sigmoid activation for normalized values (0, 1)
                 self.cnn_decoder.add_module(f"dec_tconv_bn_{i}", nn.BatchNorm2d(channels[i-1]))
                 self.cnn_decoder.add_module(f"dec_tconv_act_{i}", nn.LeakyReLU())
-            else:
-                self.cnn_decoder.add_module(f"dec_tconv_act_{i}", nn.Sigmoid())
             
     def forward(self, x: torch.Tensor):
         """
@@ -78,8 +84,12 @@ class CnnAutoencoder(nn.Module):
         x = self.mlp_encoder(x)
         x = self.mlp_decoder(x)
         x = self.cnn_decoder(x)
+
+        if not self.training or self.train_with_sigmoid:
+            x = torch.sigmoid(x)
+
         return {
             'image': x,
-            'nodes': torch.zeros(1, dtype=torch.float32),
-            'edges': torch.zeros(1, dtype=torch.float32)
+            'nodes': torch.zeros(1, dtype=torch.float32, device=x.device),
+            'edges': torch.zeros(1, dtype=torch.float32, device=x.device)
         }
